76f50cb474f7ab2e710d914c51a012dd9f9a6472
이충현 <sinna94@naver.com>
2021-09-29 15:55:38 +0900

    프로그래머스 - 소수 만들기

---
+++ src/main/java/study/programmers/level1/CreatePrimeNumber.java
@@ -0,0 +1,38 @@
+package study.programmers.level1;
+
+public class CreatePrimeNumber {
+  public int solution(int[] nums) {
+    int answer = 0;
+
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        for (int k = j + 1; k < nums.length; k++) {
+          if (isPrime(nums[i] + nums[j] + nums[k])) {
+            answer += 1;
+          }
+        }
+      }
+    }
+
+    return answer;
+  }
+
+  private boolean isPrime(int num) {
+    if (num <= 1)
+      return false;
+    if (num == 2)
+      return true;
+
+    if ((num % 2) == 0)
+      return false;
+
+    var i = 3;
+
+    while (i * i <= num) {
+      if (num % i == 0)
+        return false;
+      i++;
+    }
+    return true;
+  }
+}
---
+++ src/test/kotlin/study/programmers/level1/CreatePrimeNumberTest.kt
@@ -0,0 +1,26 @@
+package study.programmers.level1
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+class CreatePrimeNumberTest {
+  companion object {
+    @JvmStatic
+    fun args(): Stream<Arguments> {
+      return Stream.of(
+        Arguments.of(intArrayOf(1, 2, 3, 4), 1),
+        Arguments.of(intArrayOf(1, 2, 7, 6, 4), 4),
+      )
+    }
+  }
+
+
+  @ParameterizedTest
+  @MethodSource("args")
+  fun test(nums: IntArray, result: Int) {
+    Assertions.assertEquals(result, CreatePrimeNumber().solution(nums))
+  }
+}
